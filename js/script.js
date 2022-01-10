a1d8545555a096b05457cd1331efea386e038ec5
rusakroman <rrusak87@gmail.com>
2022-01-10 14:03:26 +0300

    Добавил изменения по модальному окну

--- js/script.js
+++ js/script.js
@@ -4,33 +4,64 @@ const modalBtn2 = $('.helper__button')
 
 const modalBtn3 = $('.header__button')
 
-const modalClose = $('.modal__button')
+const modalClose = $('.cross')
+
+const modalClose2 = $('.header__button')
+
+const modalOrderTitle = $('.modal__title')
+
+const modalOrderInput = $('.modal__input')
 
 modalBtn.click(function(){
-  $('.container_form2').show(1000); 
+  $('.modal').show(1000); 
 });
 
 modalBtn2.click(function(){
   console.log("Привет!");
-  $('.container_form3').show(1000); 
+  $('.modal').show(1000); 
 });
 
 modalBtn3.click(function(){
-  $('.burger-menu').show(1000); 
+  $('.burger-menu').fadeToggle(1000); 
 });
-
-modalBtn3.dblclick(function(){
-  $('.burger-menu').hide(1000); 
-});
-
 
 
 modalClose.click(function(){
-  $('.container_form2').hide(500); 
+  $('.modal').hide(500); 
 });
 
 modalClose.click(function(){
-  $('.container_form3').hide(500); 
+  $('.modal').hide(500); 
 });
 
+modalOrderInput.focus(function(){
+  modalOrderTitle
+  .text(`Введите ${$(this).attr('placeholder').toLowerCase()}`)
+})
+
+modalOrderInput.blur(function(){
+  modalOrderTitle.text('Заполните форму');
+})
+
+$('.modal').submit(function(event){
+  event.preventDefault();
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    type: 'POST',
+    data: $(this).serialize(),
+    statusCode: {
+    201: function(data) {
+      this.sucсess(data);
+      }
+    },
+    sucсess(data){
+      console.log(modalOrderTitle);
+      modalOrderTitle.text('Спасибо ваша заявка принята, номер заявки' + data.id)
+      $('modal').slideUp(300);
+    },
+    error(){
+      modalOrderTitle.text('Что-то пошло не так')
+    }
+  })
+});  
 
